95ad3f3713eef663422a099b82e5ce3b21db91da
MatthewFeaster <matthewfeaster22@gmail.com>
2024-07-06 15:20:02 +0100

    changed metadata, deleted pulgins, added .env to .gitignore

--- _data/metadata.js
+++ _data/metadata.js
@@ -1,11 +1,11 @@
 module.exports = {
-	title: "Eleventy Base Blog v8",
-	url: "https://example.com/",
+	title: "Matthew Feaster Photography",
+	url: "https://wondrous-truffle-cd990f.netlify.app/",
 	language: "en",
-	description: "I am writing about my experiences as a naval navel-gazer.",
+	description: "My photography website!",
 	author: {
-		name: "Your Name Here",
-		email: "youremailaddress@example.com",
-		url: "https://example.com/about-me/"
+		name: "Matthew Feaster",
+		email: "",
+		url: "https://wondrous-truffle-cd990f.netlify.app/"
 	}
 }
